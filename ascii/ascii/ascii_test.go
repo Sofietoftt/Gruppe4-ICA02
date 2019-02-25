aa7bdab9ca1fd3b39674c6af77644067aad246d3
Tuva Cecilie <46562790+tuvace@users.noreply.github.com>
2019-02-25 16:04:11 +0100

    Add files via upload

---
+++ ascii/ascii/ascii_test.go
@@ -0,0 +1,23 @@
+package ascii
+
+import "testing"
+import "fmt"
+
+// TestGreetingsASCII is a test
+func TestGreetingsASCII(t *testing.T) {
+	ascii := GreetingASCII()
+	if !(isASCII(ascii)) {
+		t.Fail()
+	}
+}
+
+func isASCII(s string) bool {
+	for _, C := range s {
+		if C > 127 {
+			fmt.Println("Not only ascii! ")
+			return false
+		}
+	}
+	fmt.Println("only ascii! ")
+	return true
+}
---
+++ ascii/main_ascii.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"./ascii"
+)
+
+func main() {
+
+	stringliteral := ascii.GetASCIIStringLitral()
+	ascii.IterateOverASCIIStringLiteral(stringliteral)
+	ascii.GreetingASCII()
+}
